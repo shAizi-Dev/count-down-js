08fdfe9a3c37f7f677ff3baa7e380678c1f23e55
Muhammad Shahroz <112727838+shAizi-Dev@users.noreply.github.com>
2023-09-26 07:42:13 -0700

    commit in js file end date change

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const endDate = "27 May 2023 08:20:00 PM"
+const endDate = "27 May 2024 08:20:00 PM"
 
 document.getElementById("end-date").innerText = endDate;
 const inputs = document.querySelectorAll("input")
